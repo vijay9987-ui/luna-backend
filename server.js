66cc758400de817847d9fb75bdeff44d3f6cb6b7
vijay9987-ui <nmkyvijay666@gmail.com>
2025-05-05 16:46:36 +0530

    before admin

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const dotEnv = require('dotenv');
 const categoryRoutes = require("./routes/categoryRoutes")
 const productRoutes = require("./routes/productRoutes")
-const adminRoutes = require("./routes/adminRoutes");
+
 
 
 const app = express();
@@ -37,7 +37,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 
-app.use("/api/admin", adminRoutes);
+
 
 
 
